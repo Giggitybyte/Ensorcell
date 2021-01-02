1a23918b5325799a223f89a0d78d3a39ad78e5ed
Luke Triano <luketriano115@gmail.com>
2021-01-01 22:50:39 -0800

    Initial commit

---
+++ src/main/java/me/thegiggitybyte/ensorcell/Ensorcell.java
@@ -0,0 +1,10 @@
+package me.thegiggitybyte.ensorcell;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Ensorcell implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		
+	}
+}
